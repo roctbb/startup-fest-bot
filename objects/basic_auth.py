542d76009f0d9adffa4fe196ba0744e6db6d8557
roctbb <roctbb@gmail.com>
2024-05-13 13:28:02 +0300

    Reorder login_required decorator in various routes
    The @auth.login_required decorator has been reordered for routes in payments, users, and projects blueprint files. Previous code had the decorator placed before route declaration, but this has been switched for better readability and convention following. No logic changes were performed in this commit.

--- objects/basic_auth.py
+++ objects/basic_auth.py
@@ -7,6 +7,6 @@ auth = HTTPBasicAuth()
 
 @auth.verify_password
 def verify_password(username, password):
-    print("checking")
+    print(username, password, username == ADMIN_USERNAME, check_password_hash(ADMIN_PASSWORD_HASH, password))
     if username == ADMIN_USERNAME and check_password_hash(ADMIN_PASSWORD_HASH, password):
         return username
