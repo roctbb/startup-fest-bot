4235c7040a7db1f1b008fb4ec96eeb60ebad3150
roctbb <roctbb@gmail.com>
2024-05-14 18:24:30 +0300

    Add quiz link in config and implement quiz sending
    A new environment variable QUIZ was added to the config.py file to allow for a customizable quiz link. A new file, send_quiz.py, was also created that includes a method to send this quiz link to all users who have a telegram ID. Furthermore, the pricelist.py file was renamed to send_pricelist.py to improve clarity and consistency.

--- send_quiz.py
+++ send_quiz.py
@@ -1,8 +1,11 @@
 from methods.users import *
 from config import QUIZ
 
+print(QUIZ)
+
 with app.app_context():
     users = get_all_users()
 
     for user in filter(lambda u: u.telegram_id, users):
+        print(f"sent to {user.name} - {user.telegram_id}")
         send_telegram_notification(user, f"Ссылка на квиз:\n\n{QUIZ}", )
