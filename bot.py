d440ab2e0e71a898b25a6dcbf4147e16e581888d
roctbb <roctbb@gmail.com>
2024-05-14 13:39:02 +0300

    Add quiz link in config and implement quiz sending
    A new environment variable QUIZ was added to the config.py file to allow for a customizable quiz link. A new file, send_quiz.py, was also created that includes a method to send this quiz link to all users who have a telegram ID. Furthermore, the pricelist.py file was renamed to send_pricelist.py to improve clarity and consistency.

--- bot.py
+++ bot.py
@@ -201,4 +201,8 @@ def task_handler(message, user):
 
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            print(e)
